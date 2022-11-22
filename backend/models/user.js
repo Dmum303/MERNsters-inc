20d0922368a31eb57792c14a46b2c5d2a71d4242
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-22 14:15:14 +0000

    Added back username to users

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({ 
+  userName: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
