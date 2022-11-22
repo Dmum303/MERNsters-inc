2a580b74530255fe4e12d6d7436045784ded7203
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-22 13:21:38 +0000

    Remove username field on user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({ 
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   profilePic: { type: String, required: true, default: 'https://i.imgur.com/1Q9ZQ9u.png' },
   interests: { type: Array, required: true },
   birthday: { type: Date, required: true },
