c0c67d9634c51adb92ed22c2d4c68cc88b2cab02
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-23 12:04:45 +0000

    Add footer to app

--- frontend/src/components/app/app.jsx
+++ frontend/src/components/app/app.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import Homepage from '../homepage/homepage'
 import SignUpForm from '../signup/signup'
 import {Route, Routes} from "react-router-dom";
+import Footer from '../footer/footer';
 
 const App = () => {
     return (
+      <>
       <Routes>
         <Route path='/' element={<Homepage/>}/>
         <Route path='/signup' element={<SignUpForm/>}/>
       </Routes>
-
+      <Footer />
+      </>
     );
 }
 
