a844fd8d5084c47d596bd8defc45343ecfe5b754
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-23 12:02:06 +0000

    Co-authored-by: Sarita Radia <saritahub@users.noreply.github.com> Footer

---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import './footer.css'
+
+export default function Footer () {
+    return (
+        <div className='footer'>
+            <div className='footer-links'>
+                <a href='www.facebook.com'>Facebook</a>
+                <a href='www.twitter.com'>Twitter</a>
+                <a href='www.instagram.com'>Instagram</a>
+            </div>
+        </div>
+    )
+}
