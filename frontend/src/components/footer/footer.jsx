ca24b1b03f8d78b26dde4061ace692e7f0dacafc
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-23 12:21:40 +0000

    Add font-awesome to footer

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -4,9 +4,9 @@ export default function Footer () {
     return (
         <div className='footer'>
             <div className='footer-links'>
-                <a href='www.facebook.com'>Facebook</a>
-                <a href='www.twitter.com'>Twitter</a>
-                <a href='www.instagram.com'>Instagram</a>
+                <a href='www.facebook.com'><i class="fa-brands fa-facebook fa-4x"></i></a>
+                <a href='www.twitter.com'><i class="fa-brands fa-twitter fa-4x"></i></a>
+                <a href='www.instagram.com'><i class="fa-brands fa-instagram fa-4x"></i></a>
             </div>
         </div>
     )
