6b3156a3a141c939aeaba5dd043e18a5ed5b6394
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-24 11:24:21 +0000

    Rework user signup

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,12 +1,61 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
 const User = require('../models/user');
 
-const addUser = async (req, res) => { 
-  try {
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).send('Make user');
-  } catch (error) {
-    console.log(error);
-}};
+const addUser = asyncHandler(async (req, res) => { 
+const { 
+  firstName,
+  lastName,
+  email,
+  password,
+  profilePic,
+  interests,
+  birthday,
+  gender,
+ } = req.body;
 
-module.exports = { addUser };
+ if (!firstName || !lastName || !email || !password
+  || !profilePic || !interests || !birthday || !gender) {
+    res.status(400).json({message: 'Please fill out all fields'});
+    throw new Error('Please fill out all fields');
+  }
+
+  const userExists = await User.findOne({email});
+
+  if (userExists) {
+    res.status(400).json({message: 'User already exists'});
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    password,
+    profilePic,
+    interests,
+    birthday,
+    gender,
+  })
+  user.save();
+
+res.status(201).json({
+  message: 'Make user',
+  firstName: user.firstName,
+  email: user.email,
+});
+});
+
+const getMe = asyncHandler(async (req, res) => {
+  res.send(req.user);
+})
+
+const loginUser = asyncHandler(async (req, res) => {
+  try {
+    res.send('login user');
+  } catch (error) {
+    res.status(400).send('Unable to login');
+  }
+})
+
+module.exports = { addUser, getMe, loginUser };
