39561e32980b73ec05382ed8269867e73bf1987e
Sarita <saritaradia65@gmail.com>
2022-11-24 09:55:38 +0000

    Added draft gender options to OtherInfo

--- frontend/src/components/signup/OtherInfo.jsx
+++ frontend/src/components/signup/OtherInfo.jsx
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 // import './signup.css'
 import App from '../../App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { useState } from 'react';
 
 const OtherInfo = () => {
+    const [gender, setGender] = useState("Female");
+
+    function onChangeValue(event) {
+      setGender(event.target.value);
+      console.log(event.target.value);
+    }
+
     return (
     <>
-    <div>Other info placeholder</div>
+    <div className='other-info-container'>
+        <div onChange={onChangeValue}>
+            <input type="radio" value="Female" name="gender" checked={gender === "Female"}/> Female
+            <input type="radio" value="Male" name="gender" checked={gender === "Male"} /> Male
+            <input type="radio" value="Other" name="gender" checked={gender === "Other"} /> Other
+        </div>
+    </div>
     </>
     )
 } 
--- frontend/src/components/signup/SignUpInfo.jsx
+++ frontend/src/components/signup/SignUpInfo.jsx
@@ -8,13 +8,12 @@ const SignUpInfo = () => {
     return (
     <>
     <div className='sign-up-container'>
+        <input type={'text'} placeholder={'Username'} />
         <input type={'text'} placeholder={'First Name'} />
         <input type={'text'} placeholder={'Last Name'} />
         <input type={'text'} placeholder={'Email'} />
         <input type={'text'} placeholder={'Password'} />
         <input type={'text'} placeholder={'Confirm Password'} />
-    
-
     </div>
     </>
     )
