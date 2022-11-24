5751cb38e61ac238039219c05d7650742ea5b185
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-24 21:21:55 +0000

    Add gender to post request

--- frontend/src/components/signup/OtherInfo.jsx
+++ frontend/src/components/signup/OtherInfo.jsx
@@ -1,33 +1,41 @@
-import React from 'react'
+import React from "react";
 
-import { useState } from 'react';
-
-const OtherInfo = ({formData, setFormData}) => {
-    const [gender, setGender] = useState("Female");
-
-    function onChangeValue(event) {
-      setGender(event.target.value);
-      console.log(event.target.value);
-    }
-
-    const current = new Date().toISOString().split("T")[0]
-
-
-    return (
+const OtherInfo = ({ formData, setFormData }) => {
+  function onChangeValue(event) {
+    setFormData({ ...formData, gender: event.target.value });
+  }
+  const current = new Date().toISOString().split("T")[0];
+  console.log(formData.gender);
+  return (
     <>
-    <div className='other-info-container'>
-    <input type= {'date'} placeholder={'Birthday'} max={current} />
+      <div className="other-info-container">
+        <input type={"date"} placeholder={"Birthday"} max={current} />
         <div onChange={onChangeValue}>
-            <input type="radio" value="Female" name="gender" checked={gender === "Female"}/> Female
-            <input type="radio" value="Male" name="gender" checked={gender === "Male"} /> Male
-            <input type="radio" value="Other" name="gender" checked={gender === "Other"} /> Other
+          <input
+            type="radio"
+            value="Female"
+            name="gender"
+            checked={formData.gender === "Female"}
+          />{" "}
+          Female
+          <input
+            type="radio"
+            value="Male"
+            name="gender"
+            checked={formData.gender === "Male"}
+          />{" "}
+          Male
+          <input
+            type="radio"
+            value="Other"
+            name="gender"
+            checked={formData.gender === "Other"}
+          />{" "}
+          Other
         </div>
-    </div>
-
-
-
+      </div>
     </>
-    )
-} 
+  );
+};
 
 export default OtherInfo;
