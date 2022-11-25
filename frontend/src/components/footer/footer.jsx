1f1fac2273791170cae6c9f3c6682348a658db83
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-25 09:16:25 +0000

    Remove React class warning

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -4,9 +4,9 @@ import React from "react";
 const Footer = () => (
   <div className="footer">
     <p>  
-        <a href='www.facebook.com'><i class="fa-brands fa-facebook fa-2x"swapOpacity></i></a> &nbsp;
-         <a href='www.twitter.com'><i class="fa-brands fa-twitter fa-2x"swapOpacity></i></a> &nbsp;       
-        <a href='www.instagram.com'><i class="fa-brands fa-instagram fa-2x"swapOpacity></i></a>
+        <a href='www.facebook.com'><i className="fa-brands fa-facebook fa-2x"swapOpacity></i></a> &nbsp;
+         <a href='www.twitter.com'><i className="fa-brands fa-twitter fa-2x"swapOpacity></i></a> &nbsp;       
+        <a href='www.instagram.com'><i className="fa-brands fa-instagram fa-2x"swapOpacity></i></a>
         </p>
   </div>
 );
