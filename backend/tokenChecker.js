58761c9477b376081b15fa491a844d42fe7e4dc7
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2022-11-25 16:36:18 +0000

    Removed console logs

--- backend/tokenChecker.js
+++ backend/tokenChecker.js
@@ -3,12 +3,10 @@ const tokenChecker = (req, res, next) => {
 
     let token;
     const authHeader = req.get("Authorization")
-    console.log('A')
   
     if(authHeader) {
       token = authHeader.slice(7)
     }
-    console.log('B')
     JWT.verify(token, process.env.JWT_SECRET, (err, payload) => {
       console.log(process.env.JWT_SECRET, 'Hey')
       if(err) {
@@ -19,7 +17,6 @@ const tokenChecker = (req, res, next) => {
         next();
       }
     });
-    console.log('C')
   };
 
   module.exports = tokenChecker;
