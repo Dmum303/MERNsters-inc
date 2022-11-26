6e540f7893cde2a8eb157a74952d50afb5895aa1
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-11-26 18:28:22 +0000

    Started to make components

--- frontend/src/components/chat/chat.jsx
+++ frontend/src/components/chat/chat.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
+// import MessageList from '../messagelist/messagelist';
 
 const Chat = () => {
   const [chat, setChat] = useState([]);
+  const [message, setMessage] = useState('');
   // Need a function to get post req for the current chat
   const reload = () => {
     fetch('/api/chats/findchat', {
       method: 'post',
       body: JSON.stringify({
+        // this is dummy data - needs to be made dynamic
         objectId: '6380f555e06bef726ddaf485',
       }),
       headers: {
@@ -15,12 +18,9 @@ const Chat = () => {
     })
       .then((response) => response.json())
       .then(async (data) => {
-        // Not sure that this is working when is setting the usestate
-        // const dataParse = JSON.parse(data);
-
         setChat(data);
-        // console.log(data);
-        console.log(chat);
+        console.log(chat.users[0].user.firstName);
+        console.log(chat.users);
       });
   };
 
@@ -28,21 +28,75 @@ const Chat = () => {
     reload();
   }, []);
 
+  const handleSubmit = async (error) => {
+    error.preventDefault();
+    const response = await fetch('/api/chats/addmessage', {
+      method: 'post',
+      body: JSON.stringify({
+        // this is dummy data - needs to be made dynamic
+        objectId: '6380f555e06bef726ddaf485',
+        senderId: '6380b30f83141a9fd30a7662',
+        recipientId: '6380b30f83141a9fd30a7662',
+        text: message,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        // Authorization: 'Bearer ' + token,
+      },
+    });
+    const json = await response.json();
+    if (!response.ok) {
+      console.log('Message couldnt send', json);
+    }
+    if (response.ok) {
+      // If form sent successfully then it resets the input field.
+      setMessage('');
+      reload();
+    }
+  };
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
   // Need a post func for sending a message
 
+  // When the page is reloaded from the browser it crashes because
+  // it can't read the map function, but when I refresh the page
+  // and then the chat var is popluated it works fine
+  // maybe some sort of if statement checking if the chat var is populated
+  // before returning HTML
+  // Might just need to make these into compenents and then use reload on them
   return (
     <>
       <div className="chat-container">
         <h1>Chat</h1>
         <h2>People in this chat</h2>
-
+        <p>
+          {/* {chat.users[0].user.firstName} {chat.users[0].user.lastName} */}
+        </p>
+        {/* Can't iterate over users array - not sure why? */}
+        {/* {chat.users.map((user) => {
+          <p>{user.user.firstName}</p>;
+        })} */}
         <p>Messages</p>
+        {/* <MessageList chat={chat} reload={reload} /> */}
         {chat.messages.map((message) => (
           <div class="message-container">
             <p>{message.message.text}</p>
           </div>
         ))}
-        <p>{chat._id}</p>
+        <form className="create-form-container" onSubmit={handleSubmit}>
+          <label for="message-text-box"></label>
+          <input
+            type="text"
+            className="message-text-box"
+            onChange={handleChange}
+            value={message}
+            required
+          />
+          <br></br>
+          <button class="message-btn"> message </button>
+        </form>
       </div>
     </>
   );
---
+++ frontend/src/components/messagelist/messagelist.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+// export default function MessageList({ chat, reload }) {
+//   <p>is this coming through?</p>;
+//   {
+//     chat.messages.map((message) => (
+//       <div class="message-container">
+//         <p>{message.message.text}</p>
+//       </div>
+//     ));
+//   }
+// }
