54b1ffc2dd860c08b16bd34a29333f281ca73f79
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-26 14:39:28 +0000

    Create LoginForm component

---
+++ frontend/src/components/login/loginForm.jsx
@@ -0,0 +1,52 @@
+// Login form component that returns posts an email and pass to the backend
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      email,
+      password,
+    };
+    fetch("/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+  };
+  return (
+    <div className="login-form">
+      <div className="login-title">
+        <h1>Login Form</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="login-input">
+          <input
+
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+        </div>
+        <div className="login-input">
+          <input
+
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </div>
+        <div className="login-button">
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      <div className="footer"></div>
+    </div>
+  );
+};
