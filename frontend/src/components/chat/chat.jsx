896816471ca7733ae09a6aebad4e45b594d7120d
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-11-26 13:58:14 +0000

    fetch request chat frontedn

--- frontend/src/components/chat/chat.jsx
+++ frontend/src/components/chat/chat.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 
 const Chat = () => {
-  return;
-  <>
-    <div className="chat-container">
-      <h1>Chat</h1>
-      <h2>WHut</h2>
-    </div>
-  </>;
+  const [chat, setChat] = useState([]);
+  // Need a function to get post req for the current chat
+  const reload = () => {
+    fetch('/api/chats/findchat', {
+      method: 'post',
+      body: JSON.stringify({
+        objectId: '6380f555e06bef726ddaf485',
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then(async (data) => {
+        setChat(data.chat);
+      });
+  };
+
+  useEffect(() => {
+    reload();
+  }, []);
+  console.log(chat);
+
+  // Need a post func for sending a message
+
+  return (
+    <>
+      <div className="chat-container">
+        <h1>Chat</h1>
+      </div>
+    </>
+  );
 };
 
 export default Chat;
