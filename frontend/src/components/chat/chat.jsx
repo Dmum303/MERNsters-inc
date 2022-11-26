256bb705f9e7dc48ee9474362455f8e2fd787269
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-11-26 17:22:00 +0000

    displays list of msgs

--- frontend/src/components/chat/chat.jsx
+++ frontend/src/components/chat/chat.jsx
@@ -13,16 +13,20 @@ const Chat = () => {
         'Content-Type': 'application/json',
       },
     })
-      .then((res) => res.json())
+      .then((response) => response.json())
       .then(async (data) => {
-        setChat(data.chat);
+        // Not sure that this is working when is setting the usestate
+        // const dataParse = JSON.parse(data);
+
+        setChat(data);
+        // console.log(data);
+        console.log(chat);
       });
   };
 
   useEffect(() => {
     reload();
   }, []);
-  console.log(chat);
 
   // Need a post func for sending a message
 
@@ -30,6 +34,15 @@ const Chat = () => {
     <>
       <div className="chat-container">
         <h1>Chat</h1>
+        <h2>People in this chat</h2>
+
+        <p>Messages</p>
+        {chat.messages.map((message) => (
+          <div class="message-container">
+            <p>{message.message.text}</p>
+          </div>
+        ))}
+        <p>{chat._id}</p>
       </div>
     </>
   );
