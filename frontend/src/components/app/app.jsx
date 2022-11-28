36efc41ab3ce7b880a976106de6ab2479f4f5fd8
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-11-28 12:50:10 +0000

    getusers backend built

--- frontend/src/components/app/app.jsx
+++ frontend/src/components/app/app.jsx
@@ -4,6 +4,7 @@ import Form from '../signup/Form';
 import { Route, Routes } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Chat from '../chat/chat';
+import GetUsers from '../getusers/getusers';
 import '../../App.css';
 import LoginPage from '../login/loginPage';
 
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/signup" element={<Form />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/login" element={<LoginPage />} />
+        <Routh path="/getusers" element={<GetUsers />} />
       </Routes>
       <Footer />
     </>
