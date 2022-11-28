ce5eeab5a6db719122e144f4e4b8649c0f09a04c
blakerodonnell <blakerodonnell@gmail.com>
2022-11-28 16:55:41 +0000

    Draft style for chat added

--- frontend/src/components/chat/chat.jsx
+++ frontend/src/components/chat/chat.jsx
@@ -1,4 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import NavBar from "../lib/navbar";
+import { faCaretDown, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import EmailCard from "./EmailCard";
+import "./EmailList.css";
 // import MessageList from '../messagelist/messagelist';
 
 const Chat = () => {
@@ -56,6 +61,28 @@ const Chat = () => {
     }
   };
 
+  const [emails, setEmails] = useState([
+    {
+      id: 1,
+      image: "bg-blue-100",
+      from: "Annie Lynch",
+      subject: "Aliquam erat volutpat",
+      body: `Ut id dignissim purus. Donec suscipit tortor orci, eu accumsan lectus blandit id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed vulputate ac urna ut elementum. Nunc eget metus vitae odio porta feugiat quis a mi. Vestibulum interdum maximus odio sed dignissim. Suspendisse ultricies auctor dignissim. Vivamus at lorem eget nisi ultricies scelerisque ut pellentesque erat.`,
+      hasAttachment: true,
+      time: "2:12PM",
+    },
+    {
+      id: 2,
+      image: "bg-red-100",
+      from: "Dribbble Team",
+      subject: "How are you getting on?",
+      body: `Etiam vel tincidunt lorem, vitae consequat sem. Aenean dictum nisi quis sollicitudin pharetra.Ut id dignissim purus. Donec suscipit tortor orci, eu accumsan lectus blandit id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed vulputate ac urna ut elementum. Nunc eget metus vitae odio porta feugiat quis a mi. Vestibulum interdum maximus odio sed dignissim. Suspendisse ultricies auctor dignissim. Vivamus at lorem eget nisi ultricies scelerisque ut pellentesque erat.`,
+      hasAttachment: true,
+      time: "3:30PM",
+      isSelected: true,
+    }
+  ]);
+
   const handleChange = (event) => {
     setMessage(event.target.value);
   };
@@ -67,8 +94,46 @@ const Chat = () => {
   // maybe some sort of if statement checking if the chat var is populated
   // before returning HTML
   // Might just need to make these into compenents and then use reload on them
+
+
   return (
     <>
+     <NavBar linkTo='login' />
+     <div className='parent-chat-container'>
+
+     <div className="flex flex-col bg-dark-500 w-6/12 mr-1 px-0 h-full">
+      <div className="flex items-center py-6 px-10">
+        <span className="font-light text-xl text-light-200">Inbox</span>
+        <div className="ml-2 w-5 h-4 rounded-full bg-gradient-to-br from-blue-200 to-blue-300 flex items-center justify-center text-2xs font-normal text-light-200">
+          3
+        </div>
+        <FontAwesomeIcon
+          icon={faPlus}
+          className="px-3 py-3 rounded-xl bg-gradient-to-br from-blue-200 to-blue-300 text-light-200 drop-shadow-3xl ml-auto"
+        />
+      </div>
+      <div className="px-10 pb-5">
+        <span className="text-xs text-light-200">Recent</span>
+        <FontAwesomeIcon
+          icon={faCaretDown}
+          className="text-light-200 text-xs ml-2"
+        />
+      </div>
+      <div className="flex flex-col px-10 pb-10 overflow-y-auto">
+        {emails.map((email, index) => (
+          <EmailCard key={email.id} {...email} />
+        ))}
+      </div>
+    </div>
+     {/* <div className="inbox-container">
+       "User's First Name"  <br/>
+       "Sarita"  <br/>
+       "Pauline"  <br/>
+       "Taqueer"  <br/>
+       "Arshad"  <br/>
+       "David"  <br/>
+       "Blake"
+     </div> */}
       <div className="chat-container">
         <h1>Chat</h1>
         {/* <h2>People in this chat</h2> */}
@@ -95,10 +160,12 @@ const Chat = () => {
             value={message}
             required
           />
-          <br></br>
+          
           <button className="message-btn"> message </button>
         </form>
       </div>
+      </div>
+    
     </>
   );
 };
