75d6b1024b208fa83beb75291bb3f338d730ffee
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-29 15:54:20 +0000

    Check for releasing card on right side

--- frontend/src/components/deck/card.jsx
+++ frontend/src/components/deck/card.jsx
@@ -2,12 +2,17 @@ import { useSpring, animated } from '@react-spring/web'
 import { useDrag } from '@use-gesture/react'
 
 const Card = ({ card }) => {
-  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }))
+  const [{ x}, api] = useSpring(() => ({ x: 0, y: 0 }))
 
   // Set the drag hook and define component movement based on gesture data
   const bind = useDrag(({ down, movement: [mx, my] }) => {
     api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down })
+
+    if (mx > 100 && !down) {
+      console.log('right')
+    }
   })
+
 
   return (
     <animated.div {...bind()} className="user-card" style={{ x }} >
