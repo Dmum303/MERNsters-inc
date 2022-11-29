ac073a7775923e79e6cfbd55ce0b51612cf05c1b
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-29 13:48:52 +0000

    Fine-tune card CSS

--- frontend/src/components/deck/card.jsx
+++ frontend/src/components/deck/card.jsx
@@ -1,16 +1,11 @@
 const Card = ({ card }) => {
   return (
-    // <div className='user-card'>
-    //   <h1>{card.name}</h1>
-    //   <h2>{card.email}</h2>
-    // </div>
-
     <div className="user-card">
-      <img src={card.profilePic} alt="Bby yoda" width="500px" height='300px' />
+      <img src={card.profilePic} alt={card.firstName + card.lastName} width="500px" height='300px' />
       <div className="user-card-body-container">
         <div className="user-card-body">
           <h4>
-            <b>{card.firstName}</b>
+            <b>{card.firstName} {card.lastName}</b>
           </h4>
           <p>{card.interests}</p>
         </div>
