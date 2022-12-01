1f5d1577e6d7356a0db68b318386ba76324d3821
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2022-12-01 19:11:30 +0000

    found swipe gesture

--- frontend/src/components/swipe/swipe.jsx
+++ frontend/src/components/swipe/swipe.jsx
@@ -4,8 +4,8 @@ import './swipe.css';
 export default function Swipe() {
   return (
     <div className="swipe-container">
-      <img className="swipe-right" src={"https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1200px-Smiley.svg.png"}></img>
-      <img className="swipe-left" src={"https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1200px-Smiley.svg.png"}></img>
+      <img className="swipe-right" src={"https://cdn-icons-png.flaticon.com/512/4603/4603556.png"}></img>
+      <img className="swipe-left" src={"https://cdn-icons-png.flaticon.com/512/4603/4603440.png"}></img>
       <Deck />
     </div>
   );
