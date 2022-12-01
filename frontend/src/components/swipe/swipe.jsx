21e10f7ad4f3a93d530060f8b4224513786054c8
Sarita <saritaradia65@gmail.com>
2022-12-01 11:32:03 +0000

    Added logout button for swipes page

--- frontend/src/components/swipe/swipe.jsx
+++ frontend/src/components/swipe/swipe.jsx
@@ -1,10 +1,15 @@
 import Deck from "../deck/deck";
 import './swipe.css';
+import NavBar from '../lib/navbar';
+
 
 export default function Swipe() {
   return (
+    <>
+    <NavBar linkTo="Logout" />
     <div className="swipe-container">
       <Deck />
     </div>
+    </>
   );
 }
