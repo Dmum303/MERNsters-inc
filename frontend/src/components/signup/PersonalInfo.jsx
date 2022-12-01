91fce0ae4b2ca67b10085712c07056a4760f6222
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-01 19:44:47 +0000

    Restructure interests radio buttons

--- frontend/src/components/signup/PersonalInfo.jsx
+++ frontend/src/components/signup/PersonalInfo.jsx
@@ -20,66 +20,62 @@ const PersonalInfo = ({ formData, setFormData }) => {
             type="file"
             id="userImage"
             name="filename"
-            onChange={
-              (event) => lastNoSetData(event)
-            }
+            onChange={(event) => lastNoSetData(event)}
           />
         </label>
-        <label>
-          Interest:
-          <div className="interests-container">
-            <label>
-              Heli-skiing
-              <input
-                onChange={onChangeValue}
-                type="radio"
-                value="Heli-skiing"
-                name="interest"
-                checked={formData.interests === "Heli-skiing"}
-              />
-            </label>
-            <label>
-              Quidditch
-              <input
-                onChange={onChangeValue}
-                type="radio"
-                value="Quidditch"
-                name="interest"
-                checked={formData.interests === "Quidditch"}
-              />
-            </label>
-            <label>
-              Geocaching
-              <input
-                onChange={onChangeValue}
-                type="radio"
-                value="Geocaching"
-                name="interest"
-                checked={formData.interests === "Geocaching"}
-              />
-            </label>
-            <label>
-              Ker-Plucking
-              <input
-                onChange={onChangeValue}
-                type="radio"
-                value="Ker-Plucking"
-                name="interest"
-                checked={formData.interests === "Ker-Plucking"}
-              />
-            </label>
-            <label>
-              News-raising
-              <input
-                onChange={onChangeValue}
-                type="radio"
-                value="News-raising"
-                name="interest"
-                checked={formData.interests === "News-raising"}
-              />
-            </label>
-          </div>
-        </label>
+        <p className='interests-label'>Interest:</p>
+        <div className="interests-container">
+          <label>
+            Heli-skiing
+            <input
+              onChange={onChangeValue}
+              type="radio"
+              value="Heli-skiing"
+              name="interest"
+              checked={formData.interests === "Heli-skiing"}
+            />
+          </label>
+          <label>
+            Quidditch
+            <input
+              onChange={onChangeValue}
+              type="radio"
+              value="Quidditch"
+              name="interest"
+              checked={formData.interests === "Quidditch"}
+            />
+          </label>
+          <label>
+            Geocaching
+            <input
+              onChange={onChangeValue}
+              type="radio"
+              value="Geocaching"
+              name="interest"
+              checked={formData.interests === "Geocaching"}
+            />
+          </label>
+          <label>
+            Ker-Plucking
+            <input
+              onChange={onChangeValue}
+              type="radio"
+              value="Ker-Plucking"
+              name="interest"
+              checked={formData.interests === "Ker-Plucking"}
+            />
+          </label>
+          <label>
+            News-raising
+            <input
+              onChange={onChangeValue}
+              type="radio"
+              value="News-raising"
+              name="interest"
+              checked={formData.interests === "News-raising"}
+            />
+          </label>
+        </div>
       </div>
     </>
   );
