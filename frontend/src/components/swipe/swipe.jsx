244db1f87c76965215f6bd9416de13ec1aa4de2f
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2022-12-01 12:16:05 +0000

    emojis added, both smiling

--- frontend/src/components/swipe/swipe.jsx
+++ frontend/src/components/swipe/swipe.jsx
@@ -4,6 +4,8 @@ import './swipe.css';
 export default function Swipe() {
   return (
     <div className="swipe-container">
+      <img className="swipe-right" src={"https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1200px-Smiley.svg.png"}></img>
+      <img className="swipe-left" src={"https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1200px-Smiley.svg.png"}></img>
       <Deck />
     </div>
   );
